87bdeebb4d2a1815cd7b752d6805e0faa6ccfa95
Jose Martin <jose.martin@allente.tv>
2026-01-21 12:51:26 +0100

    uppdaterar koden med hjälp av kollega på jobbet

--- test_get_products_api.py
+++ test_get_products_api.py
@@ -2,29 +2,35 @@ import requests
 import logging
 
 logging.basicConfig(level=logging.INFO)
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+}
+
 def test_products_returns_200_status_code():
-    response = requests.get("https://fakestoreapi.com/products")
+    response = requests.get("https://fakestoreapi.com/products", headers=HEADERS)
     logging.info(f"Response Status Code: {response.status_code}")
     assert response.status_code == 200, "Expected status code 200"
 
 def test_assert_amount_of_products():
-    response = requests.get("https://fakestoreapi.com/products")
+    response = requests.get("https://fakestoreapi.com/products", headers=HEADERS)
     products = response.json()
     logging.info(f"Number of Products: {len(products)}")
     assert len(products) == 20, "Expected 20 products in the response"
 
 def test_assert_specific_product_return_items():
     product_id = 2
-    response = requests.get(f"https://fakestoreapi.com/products/{product_id}")
+    response = requests.get(f"https://fakestoreapi.com/products/{product_id}", headers=HEADERS)
     product = response.json()
     logging.info(f"Title: {product['title']}, Price: {product['price']}, Category: {product['category']}")
     assert "title" in product, "Product title is missing"
     assert "price" in product, "Product price is missing"
     assert "category" in product, "Product category is missing"
+    
 
 def test_assert_specific_productId_correct_data():
     product_id = 2
-    response = requests.get(f"https://fakestoreapi.com/products/{product_id}")
+    response = requests.get(f"https://fakestoreapi.com/products/{product_id}", headers=HEADERS)
     product = response.json()
     logging.info(f"Product Data: {product}")
     assert product["id"] == product_id, f"Expected product ID to be {product_id}"
